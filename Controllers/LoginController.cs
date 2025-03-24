4f53c8ef6d6127c2f63af3db310b07863bbe26f3
akanamed <akanamed@gmail.com>
2025-03-25 01:15:38 +0900

    project init

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebServer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Login Get Method";
+        }
+
+        [HttpGet]
+        public string GetLogin()
+        {
+            return "Login GetLogin Method";
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreWebServer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Range(0.01, 10000.00)]
+        public decimal Average {  get; set; }
+    }
+}
